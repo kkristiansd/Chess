a2d278fb5050ed1a5b6b84d4404f2e975a16e9f0
Jack Spinola <jackspinola198@hotmail.com>
2023-02-13 21:40:24 +0100

    added chess piece

--- src/App.js
+++ src/App.js
@@ -18,14 +18,16 @@ function App() {
   return (
     <div className='bg-slate-400  ' style = {{height:"100%"}}>
       <div className='py-14'>
-        <p className='text-center font-sans text-2xl'>Figgity chess</p>
+        <p className='text-center font-sans text-2xl '>Figgity chess</p>
       </div>
-      <div className='drop-shadow-2xl border-5 py-4 animate-bounce	' >
+      <div className='drop-shadow-2xl border-5 py-4 ' >
         {board.map((row, index) => {
           return (
             <div className="row">
               {row.map((square, sIndex) => {
-                return <div style={{backgroundColor:square,width:80,height:80}}></div>;
+                return <div style={{backgroundColor:square,width:80,height:80}}>
+                  <img src={require('./assets/pawn.png')} height={50} width={50} style={{  display: 'block',marginLeft: 'auto',marginRight: 'auto',marginTop:7}}/>
+                </div>;
               })}
             </div>
           );
