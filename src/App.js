e399ffb5b35c3d2b8e5f54e57027d960057a3a9d
gilbertTheProgrammer <325178@VIAUC.DK>
2023-02-13 19:53:09 +0100

    chessboard

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const board = [
+  
+    ["white", "black", "white", "black","white", "black","white", "black"],
+    ["black", "white", "black", "white","black", "white","black", "white"],
+    ["white", "black", "white", "black","white", "black","white", "black"],
+    ["black", "white", "black", "white","black", "white","black", "white"],
+    ["white", "black", "white", "black","white", "black","white", "black"],
+    ["black", "white", "black", "white","black", "white","black", "white"],
+    ["white", "black", "white", "black","white", "black","white", "black"],
+    ["black", "white", "black", "white","black", "white","black", "white"],
+    
+  
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-slate-400 h-full '>
+      <div className='drop-shadow-2xl centerboard' >
+        {board.map((row, index) => {
+          return (
+            <div className="row">
+              {row.map((square, sIndex) => {
+                return <div style={{backgroundColor:square,width:80,height:80}}></div>;
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
+    
   );
+ 
 }
 
 export default App;
