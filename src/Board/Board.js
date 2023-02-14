f0f20ac31d5476a19c5fcdc277b88c3e67b46909
Jack Spinola <jackspinola198@hotmail.com>
2023-02-14 14:32:53 +0100

    board to json

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,19 +1,10 @@
 import Square from "../Square/Square";
-
+import BoardJson from "../BoardJson.json"
 
 
 function Board() {
   
-  const board = [
-    [{squareColor:'white',squarePosition:'a8',defaultPiece:'',piece:''}, {squareColor:'grey', squarePosition:'b8', defaultPiece: '', piece:''}, {squareColor: 'white', squarePosition: 'c8', defaultPiece:'', piece:''}, {squareColor: 'grey', squarePosition:'d8', defaultPiece:'', piece:''},{squareColor:'white', squarePosition: 'e8', defaultPiece:'',piece:''}, {squareColor:'grey', squarePosition:'f8', defaultPiece: '', piece:''},{squareColor:'white', squarePosition:'g8', defaultPiece:'', piece:''}, {squareColor:'grey', squarePosition:'h8', defaultPiece:'', piece:''}],
-    [{squareColor:'grey',squarePosition:'a7',defaultPiece:'bp',piece:''}, {squareColor:'white', squarePosition:'b7', defaultPiece: 'bp', piece:''}, {squareColor: 'grey', squarePosition: 'c7', defaultPiece:'bp', piece:''}, {squareColor: 'white', squarePosition:'d7', defaultPiece:'bp', piece:''},{squareColor:'grey', squarePosition: 'e7', defaultPiece:'bp',piece:''}, {squareColor:'white', squarePosition:'f7', defaultPiece: 'bp', piece:''},{squareColor:'grey', squarePosition:'g7', defaultPiece:'bp', piece:''}, {squareColor:'white', squarePosition:'h7', defaultPiece:'bp', piece:''}],
-    [{squareColor:'white',squarePosition:'a6',defaultPiece:'',piece:''}, {squareColor:'grey', squarePosition:'b6', defaultPiece: '', piece:''}, {squareColor: 'white', squarePosition: 'c6', defaultPiece:'', piece:''}, {squareColor: 'grey', squarePosition:'d6', defaultPiece:'', piece:''},{squareColor:'white', squarePosition: 'e6', defaultPiece:'',piece:''}, {squareColor:'grey', squarePosition:'f6', defaultPiece: '', piece:''},{squareColor:'white', squarePosition:'g6', defaultPiece:'', piece:''}, {squareColor:'grey', squarePosition:'h6', defaultPiece:'', piece:''}],
-    [{squareColor:'grey',squarePosition:'a5',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'b5',defaultPiece:'',piece:''}, {squareColor:'grey',squarePosition:'c5',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'d5',defaultPiece:'',piece:''},{squareColor:'grey',squarePosition:'e5',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'f5',defaultPiece:'',piece:''},{squareColor:'grey',squarePosition:'g5',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'h5',defaultPiece:'',piece:''}],
-    [{squareColor:'white',squarePosition:'a4',defaultPiece:'',piece:''}, {squareColor:'grey',squarePosition:'b4',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'c4',defaultPiece:'',piece:''}, {squareColor:'grey',squarePosition:'d4',defaultPiece:'',piece:''},{squareColor:'white',squarePosition:'e4',defaultPiece:'',piece:''}, {squareColor:'grey',squarePosition:'f4',defaultPiece:'',piece:''},{squareColor:'white',squarePosition:'g4',defaultPiece:'',piece:''}, {squareColor:'grey',squarePosition:'h4',defaultPiece:'',piece:''}],
-    [{squareColor:'grey',squarePosition:'a3',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'b3',defaultPiece:'',piece:''}, {squareColor:'grey',squarePosition:'c3',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'d3',defaultPiece:'',piece:''},{squareColor:'grey',squarePosition:'e3',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'f3',defaultPiece:'',piece:''},{squareColor:'grey',squarePosition:'g3',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'h3',defaultPiece:'',piece:''}],
-    [{squareColor:'white',squarePosition:'a2',defaultPiece:'wp',piece:'wp'}, {squareColor:'grey',squarePosition:'b2',defaultPiece:'wp',piece:'wp'}, {squareColor:'white',squarePosition:'c2',defaultPiece:'wp',piece:'wp'}, {squareColor:'grey',squarePosition:'d2',defaultPiece:'wp',piece:'wp'},{squareColor:'white',squarePosition:'e2',defaultPiece:'wp',piece:'wp'}, {squareColor:'grey',squarePosition:'f2',defaultPiece:'wp',piece:'wp'},{squareColor:'white',squarePosition:'g2',defaultPiece:'wp',piece:'wp'}, {squareColor:'grey',squarePosition:'h2',defaultPiece:'wp',piece:'wp'}],
-    [{squareColor:'grey',squarePosition:'a1',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'b1',defaultPiece:'',piece:''}, {squareColor:'grey',squarePosition:'c1',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'d1',defaultPiece:'',piece:''},{squareColor:'grey',squarePosition:'e1',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'f1',defaultPiece:'',piece:''},{squareColor:'grey',squarePosition:'g1',defaultPiece:'',piece:''}, {squareColor:'white',squarePosition:'h1',defaultPiece:'',piece:''}],
-    ];
+  const board = BoardJson;
   return (
     <>
     {board.map((row, index) => {
@@ -22,7 +13,7 @@ function Board() {
           {row.map((square, sIndex) => {
             return (
               <>
-              {console.log(square)}
+              {console.log(board)}
               <Square color={square.squareColor} position={square.squarePosition} defaultPiece={square.defaultPiece}></Square>
               </>
               
