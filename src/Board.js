6e16e3206f61976fb1133e955e0bda3660310d8e
Jack Spinola <jackspinola198@hotmail.com>
2023-02-14 07:19:52 +0100

    board js

---
+++ src/Board.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Board() {
+    
+  const board = [
+  
+    ["white", "black", "white", "black","white", "black","white", "black"],
+    ["black", "white", "black", "white","black", "white","black", "white"],
+    ["white", "black", "white", "black","white", "black","white", "black"],
+    ["black", "white", "black", "white","black", "white","black", "white"],
+    ["white", "black", "white", "black","white", "black","white", "black"],
+    ["black", "white", "black", "white","black", "white","black", "white"],
+    ["white", "black", "white", "black","white", "black","white", "black"],
+    ["black", "white", "black", "white","black", "white","black", "white"],
+    
+  
+  ];
+  return (
+    <div className='bg-slate-400  ' style = {{height:"100%"}}>
+      
+    </div>
+    
+  );
+ 
+}
+
+export default App;
