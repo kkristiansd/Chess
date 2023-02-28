75ae6d3fbd2ea41f9dbb52bb3e0855f5fa9c0b43
Jack Spinola <65230037+kokas340@users.noreply.github.com>
2023-02-28 12:56:24 +0100

    kris here take it

--- src/Piece/Pieces/Night.js
+++ src/Piece/Pieces/Night.js
@@ -3,7 +3,32 @@ import wNight from '../../assets/whiteNight.png'
 import bNight from '../../assets/blackNight.png'
 function Night(props) {
 
-    if(props.piece=="wn"){
+    const knightMoves=(position) => {
+        // Define the potential moves for the knight
+        const moves = [    [-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]
+        ];
+        
+        const validKnightMoves = [];
+        
+        // Calculate the new positions for each potential move
+        for (let move of moves) {
+          const newX = position[0] + move[0];
+          const newY = position[1] + move[1];
+          
+          if (newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7) {
+            validKnightMoves.push([newX, newY]);
+          }
+        }
+        
+        return validKnightMoves;
+      }
+      
+      // Example usage
+      const position = [7,1]; // Starting position of the knight
+      const validMoves = knightMoves(position);
+      console.log(validMoves); // Print a list of valid moves
+
+    if(props.piece==="wn"){
         return (
             <>
                         
